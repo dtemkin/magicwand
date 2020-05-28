9b3dcac5dacf57cd02312c4a297cc20a1d326209
Dan Temkin <just.dantastic@yandex.com>
2020-05-27 21:45:04 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='magicwand',
+    version='0.3.0',
+    author='Daniel Temkin',
+    description='personal utilities',
+    python_requires='>=3.2',
+    packages=['magicwand'],
+)
