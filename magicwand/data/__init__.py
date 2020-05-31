5207ff57cdb7cea93a820658d46cb05c858e98e8
dtemkin <temkin.d01@gmail.com>
2020-05-30 22:20:04 -0600

    editing struct

--- magicwand/data/__init__.py
+++ magicwand/data/__init__.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
-from collections import UserList, UserDict
+from collections import UserDict
 
 
 def conv2variable(arr, name, dtype=None):
@@ -22,6 +22,8 @@ class VariableWrapper(UserDict):
     @name.setter
     def name(self, name): self.__name__ = name
 
+    # TODO: Add transformation
+
     def __len__(self):
         return len(self['data'])
 
@@ -34,9 +36,9 @@ class VariablesContainer(UserDict):
     def set_names(self, *names, idx=0):
 
         if type(names[0]) is tuple:
-            idx, name = names.pop(0)
+            idx, name = names[0]
         else:
-            name = names.pop(0)
+            name = names[0]
 
         try:
             var = self.pop(idx)
@@ -51,11 +53,11 @@ class VariablesContainer(UserDict):
         else:
             self.set_names(*names, idx=idx+1)
 
-    def to_array(self):
+    def to_array(self): # TODO: Issue with multiple samples
 
-        return np.array([list(z) for z in
-                         zip(*[self[a]['data'].T for a in self])
-                         ])
+        return [list(z) for z in
+                zip(*[self[a]['data'].T for a in self])
+                ]
 
     def get_data(self, name):
         return self[name]['data']
@@ -68,47 +70,96 @@ class Dataset(object):
         self._y = []
         x_opts = ({} if not x_opts else x_opts)
         y_opts = ({} if not y_opts else y_opts)
+
         self._num_rows, self._num_vars = 0, 0
         self._num_samples, self._num_targets = 0, 0
-
-        if X:
+        self._x_train, self._y_train = None, None
+        self._x_test, self._y_test = None, None
+        self._x_valid, self._y_valid = None, None
+        if X is not None:
             self.set_X(vals=X, names=x_opts.get("names", None),
                        dtypes=x_opts.get("dtypes", None))
-        if y:
+        if y is not None:
             self.set_y(vals=y, names=y_opts.get("names", None),
                        dtypes=y_opts.get("dtypes", None))
 
         self._randstate = np.random.RandomState(random_seed)
 
+    @property
+    def x_train(self):
+        return self._x_train
+
+    @property
+    def y_train(self):
+        return self._y_train
+
+    @property
+    def x_test(self):
+        return self._x_test
+
+    @property
+    def y_test(self):
+        return self._y_test
+
+    @property
+    def x_valid(self):
+        return self._x_valid
+
+    @property
+    def y_valid(self):
+        return self._y_valid
+
     def get_sample(self, i):
         return self._X[i]
 
-    def train_test_split(self, test_sz, valid_sz=None,
-                         random_state=None, stratify=None, shuffle=True):
+    @staticmethod
+    def _norm_valid_size(train_sz, valid_sz):
+        return round(float(valid_sz/train_sz), 2)
+
+    def train_test_split(self, train_sz, test_sz=None, valid_sz=None,
+                         random_state=None, stratify=None, shuffle=True,
+                         X=None, y=None): # TODO: Issue with multiple samples
+
+        X = (self.get_X() if X is None else X)
+        y = (self.get_y() if y is None else y)
         rand = (self._randstate if not random_state else random_state)
         opts = {"random_state": rand, 'stratify': stratify, "shuffle": shuffle}
-
-        X_train, X_test, y_train, y_test = train_test_split(self.get_X(),
-                                                            self.get_y(),
-                                                            test_sz=test_sz,
+        test_sz = (test_sz if test_sz is not None and train_sz + test_sz <= 1.
+                   else round(1. - train_sz, 2))
+        X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                            test_size=test_sz,
+                                                            train_size=train_sz,
                                                             **opts)
-        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
-                                                          test_sz=valid_sz,
-                                                          **opts)
-
-        setattr(self, 'x_train', X_train)
-        setattr(self, 'y_train', y_train)
-        setattr(self, 'x_test', X_test)
-        setattr(self, 'y_test', y_test)
-        setattr(self, 'x_valid', X_val)
-        setattr(self, 'y_valid', y_val)
-
-    def get_X(self):
-        if len(self._X) == 1:
-            yield self._X[0].to_array()
+        if train_sz + test_sz == 1.:
+            X_val, y_val = None, None
         else:
-            for x in self._X:
-                yield x.to_array()
+
+            v_sz = (0. if not valid_sz or (train_sz + test_sz == 1.) else valid_sz)
+            v_sz = Dataset._norm_valid_size(train_sz=1-(test_sz + valid_sz),
+                                            valid_sz=v_sz)
+
+            tr_sz = round(1.-v_sz, 2)
+            v_sz = round(v_sz, 2)
+            X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
+                                                              train_size=tr_sz,
+                                                              test_size=v_sz,
+                                                              **opts)
+
+        self._x_train, self._y_train = X_train, y_train
+        self._x_test, self._y_test = X_test, y_test
+        self._x_valid, self._y_valid = X_val, y_val
+
+    def get_X(self): # TODO: Issue with multiple samples
+        arr = []
+        for x in self._X:
+            arr.extend(x.to_array())
+        return arr
+
+    def get_y(self): # TODO: Issue with multiple samples
+        arr = []
+        for y in self._y:
+            arr.extend(y.to_array())
+        return arr
 
     def _bld_contains__(self, vals_arr, names=None, dtypes=None):
 
@@ -157,13 +208,6 @@ class Dataset(object):
         self._num_rows = n
         self._num_samples = samples
         self._num_features = nfeats
-
-    def get_y(self):
-        if len(self._y) == 1:
-            yield self._y[0].to_array()
-        else:
-            for y in self._y:
-                yield y.to_array()
 
     def set_y(self, vals, names=None, dtypes=None):
         vals_arr = np.asarray(vals)
@@ -223,99 +267,4 @@ class Dataset(object):
     def num_targets(self):
         return self._num_targets
 
-
-#    def add_y_variable(self, name, data, dtype=np.float, _type="continuous"):
-#        var = VariableWrapper(name=name, data=data, dtype=dtype, _type=_type)
-#        self._y.update({var.__name__: var})
-#        mat = [list(z) for z in zip(*self._y.get('matrix'), *var.data)]
-#        self._y.update({"matrix": mat})
-    # def _apply_trans__(self, func, data, *args, **kwargs):
-    #     try:
-    #         t = func.__call__(data, *args, **kwargs)
-    #     except Exception as err:
-    #         raise Exception(f"there was an error in applying the "
-    #                         f"transformer, {err}")
-    #     else:
-    #         return t
-    #
-    # def _transform__(self, _type, transformer, in_place,
-    #                  var, axis, *args, **kwargs):
-    #     """
-    #     :param transformer: transformation function, must be callable,
-    #     must accept 2d array as 1st argument, remaining arguments passed in args,
-    #     or kwargs.
-    #     :param var: apply transformation to specific columns or rows,
-    #     (Default: None - Apply to all)
-    #     :param axis: apply transformation along specified axis
-    #     (0: row-wise, 1: col-wise)
-    #     :return: array if 'in-place' == False else None
-    #     """
-    #     if axis == 0:
-    #         data = (self.X if _type == 'x' else self.y)
-    #
-    #     elif axis == 1:
-    #         data = (self.X.T if _type == 'x' else self.y.T)
-    #
-    #     else:
-    #         raise Exception("Could not apply transformation, "
-    #                         "invalid axis value {axis}")
-    #
-    #     if var is None:
-    #         var = range(len(data))
-    #     elif type(var) == int:
-    #         var = [var]
-    #     elif type(var) == list and all([type(v)==int for v in var]):
-    #         pass
-    #     else:
-    #         raise ValueError("Invalid vars argument must be (None, int or "
-    #                          "list of int)")
-    #
-    #     transformed = []
-    #     for d in range(len(data)):
-    #         if d in var:
-    #             t = self._apply_trans__(transformer, d, *args, **kwargs)
-    #             if in_place:
-    #                 if _type == 'x' and axis == 0:
-    #                     self.X[d] = t
-    #                 elif _type == 'x' and axis == 1:
-    #                     self._X.T[d] = t
-    #                 elif _type == 'y' and axis == 0:
-    #                     self._y[d] = t
-    #                 elif _type == 'y' and axis == 1:
-    #                     self._y.T[d] = t
-    #             else:
-    #                 transformed.append(t)
-    #
-    #     return (None if in_place else transformed)
-    #
-    # def x_transform(self, transformer, in_place=True,
-    #                 var=None, axis=0, *args, **kwargs):
-    #     """
-    #     :param transformer: transformation function, must be callable,
-    #     must accept 2d array as 1st argument, remaining arguments passed in args,
-    #     or kwargs.
-    #     :param var: apply transformation to specific columns or rows,
-    #     (Default: None - Apply to all)
-    #     :param axis: apply transformation along specified axis
-    #     (0: row-wise, 1: col-wise)
-    #     :return: array if 'in-place' == False else None
-    #     """
-    #     return self._transform__(_type='x', transformer=transformer,
-    #                              in_place=in_place, var=var, axis=axis,
-    #                              args=args, kwargs=kwargs)
-    #
-    # def y_transform(self, transformer, in_place=True,
-    #                 var=None, axis=0, *args, **kwargs):
-    #     """
-    #     :param transformer: transformation function, must be callable,
-    #     must accept 2d array as 1st argument, remaining arguments passed in args,
-    #     or kwargs.
-    #     :param var: apply transformation to specific columns or rows,
-    #     (Default: None - Apply to all)
-    #     :param axis: apply transformation along specified axis
-    #     (0: row-wise, 1: col-wise)
-    #     :return: array if 'in-place' == False else None
-    #     """
-    #     return self._transform__(_type='y', transformer=transformer,
-    #                              in_place=in_place, var=var, axis=axis,
-    #                              args=args, kwargs=kwargs)
+    # TODO: Add transformation/multiplier
